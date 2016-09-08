3081a174be47d548d31f91fd29062d4abf70ad08
St. John Johnson <st.john.johnson@gmail.com>
2016-09-08 10:18:47 -0700

    Fixing non-exposed SecretFactory

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ const BuildFactory = require('./lib/buildFactory');
 const JobFactory = require('./lib/jobFactory');
 const PipelineFactory = require('./lib/pipelineFactory');
 const UserFactory = require('./lib/userFactory');
+const SecretFactory = require('./lib/secretFactory');
 
-module.exports = { BuildFactory, JobFactory, PipelineFactory, UserFactory };
+module.exports = { BuildFactory, JobFactory, PipelineFactory, SecretFactory, UserFactory };
