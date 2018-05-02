a1bf4e8f5a61b8ba86d0fc1e908dfef72e5032c3
Imgrund, Jim <jim.imgrund@gmail.com>
2018-05-02 00:01:42 -0400

    feat(966): add tests for bannerFactory create without type and isActive

--- test/lib/bannerFactory.test.js
+++ test/lib/bannerFactory.test.js
@@ -6,7 +6,7 @@ const sinon = require('sinon');
 
 sinon.assert.expose(assert, { prefix: '' });
 
-describe('Banner Factory', () => {
+describe.only('Banner Factory', () => {
     const bannerId = 123;
     const message = 'Test banner';
     const type = 'info';
@@ -83,6 +83,84 @@ describe('Banner Factory', () => {
                 assert.instanceOf(model, Banner);
                 Object.keys(bannerData).forEach((key) => {
                     assert.strictEqual(model[key], bannerData[key]);
+                });
+            });
+        });
+
+        it('should create a Banner without type defined', () => {
+            const dataWithDefaultType = Object.assign({}, bannerData);
+
+            dataWithDefaultType.type = 'info';
+            datastore.save.resolves(dataWithDefaultType);
+
+            return factory.create({
+                message,
+                isActive
+            }).then((model) => {
+                assert.isTrue(datastore.save.calledOnce);
+                assert.calledWith(datastore.save, {
+                    params: {
+                        message,
+                        type, // The bannerFactory should default this
+                        isActive
+                    },
+                    table: 'banners'
+                });
+                assert.instanceOf(model, Banner);
+                Object.keys(bannerData).forEach((key) => {
+                    assert.strictEqual(model[key], dataWithDefaultType[key]);
+                });
+            });
+        });
+
+        it('should create a Banner without isActive status defined', () => {
+            const dataWithDefaultStatus = Object.assign({}, bannerData);
+
+            dataWithDefaultStatus.isActive = true;
+            datastore.save.resolves(dataWithDefaultStatus);
+
+            return factory.create({
+                message,
+                type
+            }).then((model) => {
+                assert.isTrue(datastore.save.calledOnce);
+                assert.calledWith(datastore.save, {
+                    params: {
+                        message,
+                        type,
+                        isActive // The bannerFactory should default this
+                    },
+                    table: 'banners'
+                });
+                assert.instanceOf(model, Banner);
+                Object.keys(bannerData).forEach((key) => {
+                    assert.strictEqual(model[key], dataWithDefaultStatus[key]);
+                });
+            });
+        });
+
+        it('should create a Banner without type and isActive status defined', () => {
+            const dataWithDefaults = Object.assign({}, bannerData);
+
+            dataWithDefaults.isActive = true;
+            dataWithDefaults.type = 'info';
+            datastore.save.resolves(dataWithDefaults);
+
+            return factory.create({
+                message
+            }).then((model) => {
+                assert.isTrue(datastore.save.calledOnce);
+                assert.calledWith(datastore.save, {
+                    params: {
+                        message,
+                        type, // The bannerFactory should default this
+                        isActive // The bannerFactory should default this
+                    },
+                    table: 'banners'
+                });
+                assert.instanceOf(model, Banner);
+                Object.keys(bannerData).forEach((key) => {
+                    assert.strictEqual(model[key], dataWithDefaults[key]);
                 });
             });
         });
