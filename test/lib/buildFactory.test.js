1e76f8d0d065d1628a2fec7928ae9d106cc502c9
Jithin Emmanuel <jithine@verizonmedia.com>
2020-06-10 09:52:06 -0700

    fix: correct escaping for queries

--- test/lib/buildFactory.test.js
+++ test/lib/buildFactory.test.js
@@ -1135,7 +1135,7 @@ describe('Build Factory', () => {
             };
         });
 
-        it('return build statuses for jobs', () => {
+        it.only('return build statuses for jobs', () => {
             datastore.query.resolves(returnValue);
 
             return factory.getBuildStatuses(config).then(buildStatuses => {
